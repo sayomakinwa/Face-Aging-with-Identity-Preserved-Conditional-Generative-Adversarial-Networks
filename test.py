961352375eb2508a3ccaa01ab7ef7aeaa2ce913d
Sayo Makinwa <sayomichaelmakinwa@gmail.com>
2019-07-23 17:12:55 +0200

    Change load_test_imgs to load_imgs in test.py

--- test.py
+++ test.py
@@ -93,12 +93,12 @@ def my_train():
 
 def generate_images_from_folder(model, sess, test_data_dir=None, train_data_dir=None):
     if test_data_dir:
-        source, paths = val_generator.load_test_imgs(test_data_dir, 128)
+        source, paths = val_generator.load_imgs(test_data_dir, 128)
     else:
         source, paths = val_generator.next_source_imgs(0, 128, batch_size=256)
 
     if train_data_dir:
-        train_imgs, _ = generator.load_test_imgs(train_data_dir, 128)
+        train_imgs, _ = generator.load_imgs(train_data_dir, 128)
     else:
         train_imgs, _ = generator.next_source_imgs(0, 128, batch_size=FLAGS.batch_size-1)
 
